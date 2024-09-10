0c777e0ab35d6ccbc9235a09330521af349b56d9
SagarGidwani1 <sagargidwani08@gmail.com>
2024-09-10 07:47:23 +0000

    variable

---
+++  02_basics/variable.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+let accountCity = "Jaipur"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
