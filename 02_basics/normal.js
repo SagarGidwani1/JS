196fd3ec598ed290ec123870647b2f952ffec1cd
SagarGidwani1 <sagargidwani08@gmail.com>
2024-09-10 08:44:06 +0000

    basic

---
+++ 02_basics/normal.js
@@ -0,0 +1,33 @@
+// const accountId = 144553
+// let accountEmail = "hitesh@google.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState;
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// let isLoggedIn = "hitesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+
+//   let someNumber = 33
+//   console.log(typeof someNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+// console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+// console.log(2 == 1);
+//  console.log(2 != 1);
+
+ console.log(null > 0);
+ console.log(null == 0);
+ console.log(null >= 0);
+ 
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+ 
---
+++ 02_basics/string.js
@@ -0,0 +1,29 @@
+let name = "sagar"
+let age =18
+//console.log(`my name is ${name} and my age is ${age}`)
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+
+// const newString = gameName.substring(2)
+// console.log(newString)
+// const secondString = gameName.slice(1,4)
+// console.log(gameName)
+// console.log(secondString)
+// const thirdString = "   sagar   "
+// console.log(thirdString.trim())
+// console.log(thirdString)
+
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('hitesh')) // it check the thing is present or not
+
+console.log(gameName.split('-'));
