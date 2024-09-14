f59d840db8a7b25dbfa939e9e921a85d252acca6
SagarGidwani1 <sagargidwani08@gmail.com>
2024-09-14 13:32:38 +0000

    function

--- 03_basics/functionandscope.js
+++ 03_basics/functionandscope.js
@@ -39,3 +39,10 @@ const addTwo = function(num){
     return num + 2
 }
 
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
