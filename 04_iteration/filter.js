693be59a4287fa2ebb0f903d511165621877faee
SagarGidwani1 <sagargidwani08@gmail.com>
2024-09-15 08:52:44 +0000

    filter

---
+++ 04_iteration/filter.js
@@ -0,0 +1,47 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+// it do not return value so we use filter 
+
+const arr = [1,2,3,4,5,6,7,8,9]
+const value = arr.filter((key) => key>4 )   // output [ 5, 6, 7, 8, 9 ]
+// console.log(value)
+
+// if you want to use for each then do this
+
+const newarr = []
+arr.forEach((val)=>{
+    if(val>4){
+        newarr.push(val)
+    }
+})
+// console.log(newarr)
+
+
+//+++++++++++++++ EXAMPLE ++++++++++++++//
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  //let user = books.filter((bk) => bk.genre==='Non-Fiction')
+
+ let user = books.filter((bk)=>{
+    return bk.genre==='History' && bk.publish >= 1980    // we can apply condition also
+  })
+  console.log(user)
